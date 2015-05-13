eefe8dda07fc858d2d3315de5af8b43402df87b0
Antti Kuosmanen <antti@seravo.fi>
2015-05-13 22:50:22 +0300

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== WP Libre Form ===
+Contributors: Zuige
+Tags: Contact, Form, Lead, Collector
+Donate link: http://seravo.fi/
+Requires at least: 4.2.2
+Tested up to: 4.2.2
+Stable tag: 0.1
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+== Description ==
+
+A better contact form builder and lead collector
+
+== Installation ==
+
+1. Upload plugin to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the "Plugins" menu in WordPress.
+3. Installation done!
+
+== Frequently Asked Questions ==
+
+None yet.
+
+== Screenshots ==
+
+None yet.
+
+== Changelog ==
+
+Note that complete commit log is available at https://github.com/Seravo/wp-libre-form/commits/master
+
+== Upgrade Notice ==
+
---
+++ wp-libre-form.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin name: WP Libre Form
+ * Plugin URI: https://github.com/Seravo/wp-libre-form
+ * Description: A better contact form builder and lead collector
+ * Version: 0.1
+ * Author: Seravo Oy
+ * Author: http://seravo.fi
+ * License: GPLv3
+ *
+ * This plugin acts as a simple, no bullshit form maker for WordPress installations.
+ * Everything is created with WordPress core functionality only, so it's beautiful.
+ *
+ * Go ahead, just look at the code! :)
+ */
+
+/** Copyright 2015 Seravo Oy
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 3, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
