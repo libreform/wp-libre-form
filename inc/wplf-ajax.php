9a5bd7ef4410deaa6f0aa7a87bd56f55fc73241a
Antti Kuosmanen <antti@seravo.fi>
2015-08-20 12:17:45 +0300

    Add some actions for end-users

--- inc/wplf-ajax.php
+++ inc/wplf-ajax.php
@@ -27,13 +27,16 @@ function wplf_ajax_submit_handler() {
   $return = new stdClass();
   $return->ok = 1;
 
+  // allow user to pre-process the post fields
+  do_action('wplf_pre_validate_submission');
+
   // validate form fields
   // see wplf-form-validation.php
   $return = apply_filters( 'wplf_validate_submission', $return );
 
   if( $return->ok ) {
 
-    // form existance has  already been validated via filters
+    // form existence has already been validated via filters
     $form = get_post( $_POST['_form_id'] );
 
     // the title is the value of whatever the first field was in the form
@@ -73,6 +76,10 @@ function wplf_ajax_submit_handler() {
 
     // return the success message for the form
     $return->success = apply_filters( 'the_content', get_post_meta( $form->ID, '_wplf_thank_you', true ) );
+
+    // allow user to attach custom actions after the submission has been received
+    // these could be confirmation emails, additional processing for the submission fields, e.g.
+    do_action('wplf_post_validate_submission', $return);
 
   }
 
