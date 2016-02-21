62f7c6f32dbca7d0c70e2b4595e94ab7bda388de
Antti Kuosmanen <antti@seravo.fi>
2016-02-21 23:34:32 +0100

    Bump version to 0.2

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: Contact, Form, Lead, Collector
 Donate link: https://github.com/anttiviljami
 Requires at least: 4.2
 Tested up to: 4.4.2
-Stable tag: 0.1
+Stable tag: 0.2
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
--- wp-libre-form.php
+++ wp-libre-form.php
@@ -3,7 +3,7 @@
  * Plugin name: WP Libre Form
  * Plugin URI: https://github.com/anttiviljami/wp-libre-form
  * Description: HTML form builder for WordPress
- * Version: 0.1
+ * Version: 0.2
  * Author: @anttiviljami
  * Author URI: https://github.com/anttiviljami/
  * License: GPLv3
