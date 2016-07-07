e6e5651d4273ad1f1c76d9c6cde1ceae26ba95c2
Antti Kuosmanen <antti@seravo.fi>
2016-07-07 19:58:43 +0300

    Tagging version 1.0.2

--- wp-libre-form.php
+++ wp-libre-form.php
@@ -3,7 +3,7 @@
  * Plugin name: WP Libre Form
  * Plugin URI: https://github.com/anttiviljami/wp-libre-form
  * Description: A minimal HTML form builder for WordPress; made for developers
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: @anttiviljami
  * Author URI: https://github.com/anttiviljami/
  * License: GPLv3
