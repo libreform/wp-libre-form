c3fd159a14f4fbefe7054661f979299dc469e233
Jani Peltoniemi <jani@oliomedia.fi>
2016-11-23 15:55:48 +0200

    More or less complete overhaul of wplf-ajax and wplf-form-actions
    This is based on #40 but a lot has changed
    
    * Data is mostly handled in wplf-ajax and passed forward as a single array
    * Added various filters
    * HTML content detection and automatic headers
    
    See comments for other changes

--- inc/wplf-ajax.php
+++ inc/wplf-ajax.php
@@ -6,6 +6,7 @@
 add_action( 'wp_ajax_wplf_submit', 'wplf_ajax_submit_handler' );
 add_action( 'wp_ajax_nopriv_wplf_submit', 'wplf_ajax_submit_handler' );
 function wplf_ajax_submit_handler() {
+  $wplf = WP_Libre_Form::init();
   $return = new stdClass();
   $return->ok = 1;
 
@@ -17,34 +18,58 @@ function wplf_ajax_submit_handler() {
   $return = apply_filters( 'wplf_validate_submission', $return );
 
   if( $return->ok ) {
+    // copy $_POST to $data in order to undo WordPress' magic quotes for
+    // wplf_post_data. Filtered data will be escaped appropriately.
+    $data = stripslashes_deep($_POST);
+
     // form existence has already been validated via filters
+    $form = get_post( intval( $data['_form_id'] ) );
 
-    // copy $_POST to $form_data in order to undo WordPress' magic quotes
-    $form_data = stripslashes_deep($_POST);
 
-    // Make form data filterable
-    $form_data = apply_filters( 'wplf_form_data', $form_data );
+    // Make POST data filterable
+    $data = apply_filters( 'wplf_submission_data', $data );
 
-    $form = get_post( intval( $form_data['_form_id'] ) );
+    $form_meta = get_post_meta( $form->ID );
 
-    $title_format = get_post_meta( $form->ID, '_wplf_title_format', true );
+    // Compile all relevant data into a single array. This will be passed to wplf_post_validate_submission,
+    // so the same data wouldn't have to be looked up again.
+    // This is mostly for aesthetic reasons and DRY KISS.
+    $submission = [
+      'data' => $data,      // Filtered POST data
+      'meta' => [],         // Form metadata
+      'form' => $form,      // Form object
+      'post_title' => '',   // Filtered submission title
+    ];
+    // Seems that all metadata have only 1 value, so we simplify the structure a bit.
+    // TODO: Just to be sure, here's a reminder to check
+    foreach ($form_meta as $key => $value) {
+      $submission['meta'][$key] = $value[0];
+    }
 
-    // substitute the %..% tags with field values
-    $post_title = $title_format;
+    // Try _wplf_title_format from metadata but if it for some reason fails, just use form name
+    $title_format = !empty($submission['meta']['_wplf_title_format']) ? $submission['meta']['_wplf_title_format'] : $form->post_title;
+    $title_format = apply_filters('wplf_submission_title_format', $title_format);
 
-    $wplf = WP_Libre_Form::init();
-    $post_title = $wplf->substitute($post_title, $form_data);
+
+    // special values for subtitution that need an easier key are not really usable elsewhere
+    $specialValues = [
+      'form-name' => $form->post_title,
+    ];
+      // Assign submission title and substitute tokens
+    $submission['post_title'] = $wplf->substitute($title_format, $specialValues, false);
+    $submission['post_title'] = $wplf->substitute($submission['post_title'], $submission['data']);
+    // Probably no extra value in filtering post title, since $title_format has a filter already, so skip it.
 
     // create submission post
     $post_id = wp_insert_post( array(
-      'post_title'     => $post_title,
+      'post_title'     => $submission['post_title'],
       'post_status'    => 'publish',
       'post_type'      => 'wplf-submission',
     ));
 
     // add submission data as meta values
-    foreach( $form_data as $key => $value ) {
-      if ( $slash = is_array($value ) )
+    foreach( $submission['data'] as $key => $value ) {
+      if ( is_array($value ) )
         $value = json_encode($value);
 
       // Escape slashes for add_post_meta (https://codex.wordpress.org/Function_Reference/update_post_meta#Character_Escaping)
@@ -60,20 +85,25 @@ function wplf_ajax_submit_handler() {
       $attach_id = media_handle_upload( $key, 0, array(), array( "test_form" => false ) );
       add_post_meta( $post_id, $key, wp_get_attachment_url($attach_id) );
       add_post_meta( $post_id, $key . "_attachment", $attach_id );
+
+      // TODO: For completeness' sake attachments should also be added to submission
     }
 
 
 
     $return->submission_id = $post_id;
-    $return->submission_title = $post_title;
+    $return->submission_title = $submission['post_title'];
     $return->form_id = $form->ID;
 
     // return the success message for the form
-    $return->success = apply_filters( 'the_content', get_post_meta( $form->ID, '_wplf_thank_you', true ) );
+    $return->success = apply_filters( 'the_content', $submission['meta']['_wplf_thank_you'], true );
+
+    if (WP_DEBUG)
+      $return->debug = $submission;
 
     // allow user to attach custom actions after the submission has been received
     // these could be confirmation emails, additional processing for the submission fields, e.g.
-    do_action('wplf_post_validate_submission', $return);
+    do_action('wplf_post_validate_submission', $return, $submission);
 
   }
 
--- inc/wplf-form-actions.php
+++ inc/wplf-form-actions.php
@@ -3,40 +3,88 @@
 /**
  * Send a copy of the form fields email if feature is enabled
  */
-add_action( 'wplf_post_validate_submission', 'wplf_send_email_copy', 20 );
-function wplf_send_email_copy( $return ) {
+add_action( 'wplf_post_validate_submission', 'wplf_send_email_copy', 20, 2 );
+function wplf_send_email_copy( $return, $submission ) {
+  $wplf = WP_Libre_Form::init();
   // do nothing if form validation failed
-  if( ! $return->ok ) {
+  if( ! $return->ok )
     return;
+
+
+  // do nothing if email is disabled
+  if( !isset( $submission['meta']['_wplf_email_copy_enabled'] ) || !$submission['meta']['_wplf_email_copy_enabled'][0] )
+    return;
+
+  $submission['email'] = [
+    'headers' => [],
+    'charset' => 'UTF-8',
+    'mime' => ''
+  ];
+  // Email specific filtering here
+
+  $subject_format = isset( $submission['meta']['_wplf_email_copy_subject_format'] )
+    ? $submission['meta']['_wplf_email_copy_subject_format']
+    : '%title% - New submission from %name%';
+
+  $subject_format = apply_filters( 'wplf_email_copy_subject_format', $subject_format );
+
+  // special values for subtitution that need an easier key are not really usable elsewhere
+  $special_values = [
+    'form-name' => $submission['form']->post_title,
+  ];
+    // Assign submission title and substitute tokens
+  $submission['email']['subject'] = $wplf->substitute($subject_format, $special_values, false);
+  $submission['email']['subject'] = $wplf->substitute($submission['email']['subject'] , $submission['data'] );
+
+  // Locate and render template
+
+  $template_file = !empty( $submission['meta']['_wplf_email_copy_template'] ) ? $submission['meta']['_wplf_email_copy_template'] : '';
+  // Don't use locate_template(). This is easier.
+  $templates = $wplf->get_email_templates();
+
+  // If chosen template can't for some reason be found, use default.
+  if ( !isset( $templates[$template_file] ) )
+    $template_file = '_wplf/default.php';
+
+  $template = $templates[$template_file];
+
+  // Extract submission data to local scope for the template
+  extract( $submission );
+  ob_start();
+  require( $template['path'] );
+  $content = ob_get_clean();
+
+
+  if (!$submission['email']['mime']) {
+    // Try a filter to detect html content
+    $is_html = apply_filters( 'wplf_email_copy_detect_html', null, $content, $submission);
+
+    // If $is_html is still null(no filters / no html detected), try to match /<html.*>/
+    if ($is_html === null)
+      $is_html = preg_match('/<html(\s+.*)?>/', $content); // Content is probably a html document
+
+    if ($is_html !== null)
+      $submission['email']['mime'] = $is_html ? 'text/html' : 'text/plain';
+  }
+  $submission['email']['headers']['Content-Type'] = $submission['email']['mime'] . ';charset=' . $submission['email']['charset'];
+
+  // Normalize headers
+  foreach ($submission['email']['headers'] as $key => $value) {
+    if (!is_int($key))
+      $value = $key . ': ' . $value;
+
+    $submission['email']['headers'] = $value;
   }
 
-  $form_id = intval( $_POST['_form_id'] ); // _form_id is already validated and we know it exists by this point
-  $form_title = esc_html( get_the_title( $form_id ) );
-  $form_meta = get_post_meta( $form_id );
-  $referrer = esc_url_raw( $_POST['referrer'] );
+  // Last chance to filter $submission
+  $submission = apply_filters( 'wplf_email_submission_send', $submission);
 
-  if( isset($form_meta['_wplf_email_copy_enabled']) && $form_meta['_wplf_email_copy_enabled'][0] ) {
-    $to = isset($form_meta['_wplf_email_copy_to']) ? $form_meta['_wplf_email_copy_to'][0] : get_option( 'admin_email' );
-    $subject = wp_sprintf( __('New submission from %s', 'wp-libre-form'), $referrer );
-    $content = wp_sprintf( __('Form "%s" (ID %d) was submitted with values below: ', 'wp-libre-form'), $form_title, $form_id );
-    $content = apply_filters( 'wplf_email_copy_content_start', $content, $form_title, $form_id ). "\n\n";
+  wp_mail(
+    apply_filters( 'wplf_email_copy_to', $submission['meta']['_wplf_email_copy_to'], $submission ),
+    apply_filters( 'wplf_email_copy_subject', $submission['email']['subject'], $submission ),
+    apply_filters( 'wplf_email_copy_content', $content, $submission ),
+    apply_filters( 'wplf_email_copy_headers', $submission['email']['headers'], $submission ),
+    apply_filters( 'wplf_email_copy_attachments', array(), $submission )
+  );
 
-    foreach( $_POST as $key => $value ) {
-      if( '_' === $key[0] ) {
-        continue;
-      }
-      if( is_array( $value ) ) { // in case input type="radio" submits an array
-        $value = implode( ', ', $value );
-      }
-      $content .= esc_html( $key ) . ': ' . esc_html( print_r( $value, true ) ) . "\n";
-    }
-
-    wp_mail(
-      apply_filters( 'wplf_email_copy_to', $to ),
-      apply_filters( 'wplf_email_copy_subject', $subject ),
-      apply_filters( 'wplf_email_copy_content', $content ),
-      apply_filters( 'wplf_email_copy_headers', '' ),
-      apply_filters( 'wplf_email_copy_attachments', array() )
-    );
-  }
 }
