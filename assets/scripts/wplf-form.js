6abefa1b3d1f90867a381001b1353c8b907c3b15
Christian Nikkanen <me@kisu.li>
2017-02-03 11:09:54 +0200

    Add fallback for credentials

--- assets/scripts/wplf-form.js
+++ assets/scripts/wplf-form.js
@@ -26,7 +26,7 @@
 
       fetch(ajax_object.ajax_url  + '?action=wplf_submit', {
         method: "POST",
-        credentials: ajax_object.ajax_credentials,
+        credentials: ajax_object.ajax_credentials || 'omit', // if it's not defined, omit. 
         body: data
       }).then(function(response) {
         return response.text();
