b08a6ff0c48b8640fa6bc945507461eb1df8ed5d
henrikrinne <hencca77@gmail.com>
2018-06-07 11:14:31 +0300

    wrapped frontend JS in IIFE

--- assets/scripts/wplf-form.js
+++ assets/scripts/wplf-form.js
@@ -2,6 +2,8 @@
  * JS to be included on the front end pages with forms
  */
 
+
+
 window.wplf = {
   successCallbacks: [],
   errorCallbacks: [],
@@ -15,21 +17,21 @@ window.wplf = {
     // add class to enable css changes to indicate ajax loading
     form.classList.add("sending");
 
-    [].forEach.call(form.querySelectorAll(".wplf-error"), function(error) {
+    [].forEach.call(form.querySelectorAll(".wplf-error"), function (error) {
       // reset errors
       error.parentNode.removeChild(error);
     });
 
-    fetch(ajax_object.ajax_url  + '?action=wplf_submit', {
+    fetch(ajax_object.ajax_url + '?action=wplf_submit', {
       method: "POST",
       credentials: ajax_object.ajax_credentials || 'same-origin',
       body: data
-    }).then(function(response) {
+    }).then(function (response) {
       return response.text();
-    }).then(function(response) {
+    }).then(function (response) {
       response = JSON.parse(response);
 
-      if( 'success' in response ) {
+      if ('success' in response) {
         // show success message if one exists
         var success = document.createElement("p");
         success.className = "wplf-success";
@@ -38,16 +40,16 @@ window.wplf = {
         form.parentNode.insertBefore(success, form.nextSibling);
       }
 
-      if( 'ok' in response && response.ok ) {
+      if ('ok' in response && response.ok) {
         // submit succesful!
         form.parentNode.removeChild(form);
 
-        window.wplf.successCallbacks.forEach(function(func) {
+        window.wplf.successCallbacks.forEach(function (func) {
           func(response);
         });
       }
 
-      if( 'error' in response ) {
+      if ('error' in response) {
         // show error message in form
         var error = document.createElement("p");
         error.className = "wplf-error error";
@@ -55,17 +57,17 @@ window.wplf = {
 
         form.appendChild(error);
 
-        window.wplf.errorCallbacks.forEach(function(func) {
+        window.wplf.errorCallbacks.forEach(function (func) {
           func(response);
         });
       }
 
       form.classList.remove('sending');
-    }).catch(function(error) {
+    }).catch(function (error) {
       form.classList.remove("sending");
 
       if (window.wplf.errorCallbacks.length > 0) {
-        window.wplf.errorCallbacks.forEach(function(func) {
+        window.wplf.errorCallbacks.forEach(function (func) {
           func(error);
         });
       } else {
@@ -76,7 +78,7 @@ window.wplf = {
     // don't actually submit the form, causing a page reload
     e.preventDefault();
   },
-  attach: function(form) {
+  attach: function (form) {
     // form is a selector
     if (typeof form == 'string') {
       form = document.querySelectorAll(form);
@@ -84,7 +86,7 @@ window.wplf = {
 
     // form is an array of elements or a node list
     if (form.constructor === Array || form.constructor === NodeList) {
-      [].forEach.call(form, function(form){
+      [].forEach.call(form, function (form) {
         window.wplf.attach(form);
       });
       return;
@@ -94,55 +96,60 @@ window.wplf = {
   }
 };
 
-var main = function () {
-  [].forEach.call(document.querySelectorAll(".libre-form"), window.wplf.attach);
-};
 
-var dependencies = [];
+(function () {
 
-if (!window.fetch) {
-  dependencies.push('fetch');
-}
-
-if (!window.Promise) {
-  dependencies.push('Promise');
-}
-
-
-function initialize(dependencies, app) {
-  var dependency_count = dependencies.length;
-  var dependencies_loaded = 0;
-  var is_ready = function() {
-    return dependency_count === dependencies_loaded;
-  };
-  var run_when_ready = function() {
-    if (is_ready()) {
-      app();
-    }
-  };
-  var get_script = function(script) {
-    var elmnt = document.createElement('script');
-    elmnt.src = ajax_object.wplf_assets_dir + '/scripts/polyfills/' + script + '.js';
-
-    return elmnt;
+  var main = function () {
+    [].forEach.call(document.querySelectorAll(".libre-form"), window.wplf.attach);
   };
 
-  dependencies.map(function(dependency) {
-    return dependency.toLowerCase();
-  }).map(function(dependency) {
-    return get_script(dependency);
-  }).map(function(script) {
-    script.addEventListener('load', function() {
-      dependencies_loaded++;
-      run_when_ready();
+  var dependencies = [];
+
+  if (!window.fetch) {
+    dependencies.push('fetch');
+  }
+
+  if (!window.Promise) {
+    dependencies.push('Promise');
+  }
+
+
+  function initialize(dependencies, app) {
+    var dependency_count = dependencies.length;
+    var dependencies_loaded = 0;
+    var is_ready = function () {
+      return dependency_count === dependencies_loaded;
+    };
+    var run_when_ready = function () {
+      if (is_ready()) {
+        app();
+      }
+    };
+    var get_script = function (script) {
+      var elmnt = document.createElement('script');
+      elmnt.src = ajax_object.wplf_assets_dir + '/scripts/polyfills/' + script + '.js';
+
+      return elmnt;
+    };
+
+    dependencies.map(function (dependency) {
+      return dependency.toLowerCase();
+    }).map(function (dependency) {
+      return get_script(dependency);
+    }).map(function (script) {
+      script.addEventListener('load', function () {
+        dependencies_loaded++;
+        run_when_ready();
+      });
+
+      return document.body.appendChild(script);
     });
 
-    return document.body.appendChild(script);
+    run_when_ready();
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    initialize(dependencies, main);
   });
 
-  run_when_ready();
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  initialize(dependencies, main);
-});
+}());
